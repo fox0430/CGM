535b96e35b23e7c72ade772f965f7a318325a801
fox0430 <ipoduser0430@gmail.com>
2019-06-28 17:56:53 +0900

    Many fix

--- src/calcutils.nim
+++ src/calcutils.nim
@@ -1,48 +1,26 @@
 import math
 
-type Matrix* = seq[seq[float64]]
+type
+  Matrix* = seq[seq[float32]]
+  Vector* = seq[float32]
 
-proc vectorNorm*(m: Matrix): float64 =
-  for i in 0 ..< m.len:
-    result = result + m[i][0].abs
-  result = sqrt(result)
+proc `*`*(m: Matrix, v: Vector): Vector =
+  for i in 0 ..< v.len:
+    result.add(0'f32)
+    for j in 0 ..< v.len: result[i] = result[i] + m[i][j] * v[j]
 
-proc `*`*(a, b: Matrix): Matrix =
-  for i in 0 ..< a.len:
-    result.add(@[])
-    for j in 0 ..< b[0].len:
-      result[i].add(@[float64(0)])
-      for k in 0 ..< a[0].len:
-        result[i][j] = result[i][j] + (a[i][k] * b[k][j])
+proc `-`*(v1, v2: Vector): Vector =
+  for i in 0 ..< v1.len: result.add(v1[i] - v2[i])
 
-proc `*`*(m: Matrix, f: float64): Matrix =
-  for i in 0 ..< m.len:
-    result.add(@[])
-    for j in 0 ..< m[0].len:
-      result[i].add(m[i][j] * f)
+proc `*`*(v1, v2: Vector): float32 =
+  for i in 0 ..< v1.len: result = result + v1[i] * v2[i]
 
-proc `+`*(a, b: Matrix): Matrix =
-  for i in 0 ..< a.len:
-    result.add(@[])
-    for j in 0 ..< a[0].len:
-      result[i].add(a[i][j] + b[i][j])
+proc `*`*(f: float32, v: Vector): Vector =
+  for i in 0 ..< v.len: result.add(f * v[i])
 
-proc `-`*(a, b: Matrix): Matrix =
-  for i in 0 ..< a.len:
-    result.add(@[])
-    for j in 0 ..< a[i].len:
-      result[i].add(a[i][j] - b[i][j])
+proc `+`*(v1, v2: Vector): Vector =
+  for i in 0 ..< v1.len: result.add(v1[i] + v2[i])
 
-proc `/`*(a, b: Matrix): Matrix =
-  for i in 0 ..< a.len:
-    result.add(@[])
-    for j in 0 ..< a[0].len:
-      result[i].add(a[i][j] / b[i][j])
-
-proc T*(m: Matrix): Matrix =
-  for i in 0 ..< m[0].len:
-    result.add(@[])
-    for j in 0 ..< m.len:
-      result[i].add(m[j][i])
-
-
+proc vectorNorm*(v: Vector): float32 =
+  for i in 0 ..< v.len: result = result + v[i] * v[i]
+  return sqrt(result)
--- src/cgm.nim
+++ src/cgm.nim
@@ -1,26 +1,64 @@
+import math
 import calcutils
 
-proc CGSolver(A, b, x0: Matrix): Matrix =
+proc CGSolver(A: Matrix, b: Vector, x: var Vector): Vector =
   var
-    x = x0
-    r0 = b - A * x0
-    p = r0
+    r = b - A * x
+    p = r
+    x1: Vector
 
-  for i in 0 ..< 200:
+  for i in 0 ..< 10000000:
     let
-      a = ((r0.T * r0) / ((p.T * A) * p))[0][0]
-      r1 = r0 - (A * a) * p
-    x = x + p * a
-    echo "Error: " & $r1.vectorNorm
-    if r1.vectorNorm < 10e-10: return x
+      Ap = A * p
+      alpha = (r * r) / (p * Ap)
+
+    x1 = x + alpha * p
+
+    let r1 = r - alpha * Ap
+
+    let error = r1.vectorNorm / b.vectorNorm
+    #echo error
+    if error < 10e-10: return x1
+
+    let beta = (r1 * r1) / (r * r)
+    p = r1 + beta * p
+
+    r = r1
+    x = x1
+
+  return x1
+
+proc initMatrixA(alpha, beta: float32, n: int): Matrix =
+  for i in 0 ..< n - 1:
+    result.add(newSeq[float32](n - 1))
+    if i == 0:
+      result[i][0] = alpha
+      result[i][1] = beta
     else:
-      let b = ((r1.T * r1) / ((r0.T * A) * p))
-      p = r1 + p * b
-      r0 = r1
-  return x
+      result[i][i - 1] = beta
+      if i < n - 1: result[i][i] = alpha
+      if i + 1 < n - 1: result[i][i + 1] = beta
+
+  #for i in 0 ..< result.len: echo result[i]
+
+proc initVectorb(beta: float32, n: int): Vector =
+  result = newSeq[float32](n - 1)
+  for i in 0 ..< n - 1:
+    if i == 0 or i == n - 2: result[i] = 4
+    else: result[i] = 5
+
+  # echo result
 
 when isMainModule:
-  const A:Matrix = @[@[1'f64, 0'f64, 0'f64], @[0'f64, 2'f64, 0'f64], @[0'f64, 0'f64, 1'f64]]
-  const b:Matrix = @[@[4'f64], @[5'f64], @[6'f64]]
-  const x:Matrix = @[@[0'f64], @[0'f64], @[0'f64]]
-  echo "Anser:" & $CGSolver(A, b, x )
+  const n = 1000
+
+  let
+    #deltaX = PI / 2 / n
+    A = initMatrixA(3, 1, n)
+    b = initVectorb(4, n)
+  var x: Vector = newSeq[float32](n - 1)
+
+  let ans = CGSolver(A, b, x)
+
+  echo "ans"
+  echo ans
